22efb2ab00485174496a63eb576bcd8861fc2b50
Zhixing Feng <zhixingfeng@zhixingdemacbook-pro.local>
2017-03-22 23:20:30 -0400

    Initial Commit

---
+++ splitreadsbymovie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  splitreadsbymovie
+//
+//  Created by Zhixing Feng on 17/3/22.
+//  Copyright © 2017年 Icahn School of Medicine at Mount Sinai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
